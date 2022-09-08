548b1b37cbbfab5427f93d87e54f309aea93dd33
Jack <jack.allen@above-uav.com>
2022-09-08 02:43:59 -0700

    improve example helper functions

--- examples/ex_helpers.hpp
+++ examples/ex_helpers.hpp
@@ -1,24 +1,52 @@
 #include <thread>
 #include <chrono>
 #include <sstream>
+#include <iomanip>
 #include <iostream>
 
 using std::this_thread::sleep_for;
 using namespace std::chrono_literals;
 
+static std::mutex cout_mtx;
+static std::chrono::steady_clock::time_point genesis {
+        std::chrono::steady_clock::now()};
+
+template <typename last_t>
+inline void variadic_strcat(std::stringstream& stream,
+        const last_t last)
+{
+    stream << last;
+}
+
+template <typename current_t, typename next_t, typename... remainder_t>
+inline void variadic_strcat(std::stringstream& stream, 
+        const current_t current, const next_t next, 
+        const remainder_t... remainder)
+{
+    stream << current;
+    variadic_strcat(stream, next, remainder...);
+}
+
+template <typename... str_args>
+inline std::string make_str(const str_args... args)
+{
+    std::stringstream ss;
+    variadic_strcat(ss, args...);
+    return ss.str();
+}
+
 std::string timepoint_string()
 {
     std::stringstream tpss;
-    tpss.precision(8);
-    auto tp = std::chrono::steady_clock::now()
-            .time_since_epoch().count();
-    tpss << '[' << (double)tp / 1000000000.0 << ']';
+    tpss << '[' << std::setfill('0') << std::setw(5) <<
+            (std::chrono::steady_clock::now() -
+            genesis).count() / 1000000 << ']';
     return tpss.str();
 }
 
-void log(const char* msg)
+template <typename... str_args>
+void log(const str_args... va_args)
 {
-    static std::mutex cout_mtx;
     std::lock_guard<std::mutex> guard {cout_mtx};
-    std::cout << timepoint_string() << ' ' << msg << '\n';
+    std::cout << timepoint_string() << ' ' << make_str(va_args...) << '\n';
 }
